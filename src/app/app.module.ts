952e9842bc33c5d787691c6e16da9e2a240ce5f4
lucasNasc <lucasfortunato328@gmail.com>
2025-11-18 12:42:40 -0300

    configuracoes iniciais e de banco de dados local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import appConfig from './app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ConfigModule.forFeature(appConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(appConfig)],
+      inject: [appConfig.KEY],
+      useFactory: async (appConfigParam: ConfigType<typeof appConfig>) => {
+        return {
+          type: appConfigParam.database.type,
+          host: appConfigParam.database.host,
+          port: appConfigParam.database.port,
+          username: appConfigParam.database.username,
+          database: appConfigParam.database.database,
+          password: appConfigParam.database.password,
+          autoLoadEntities: appConfigParam.database.autoLoadEntities,
+          synchronize: appConfigParam.database.synchronize,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
