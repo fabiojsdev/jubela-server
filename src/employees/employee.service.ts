0d6ff01d82d596634db9d935e6e8e08c65294979
lucasNasc <lucasfortunato328@gmail.com>
2025-11-19 14:54:40 -0300

    adicionando metodos no service de funcionarios

--- src/employees/employee.service.ts
+++ src/employees/employee.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HashingServiceProtocol } from 'src/auth/hashing/hashing.service';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { CreateEmployeeDTO } from './dto/create-employee.dto';
+import { PaginationDTO } from './dto/pagination-employee.dto';
+import { UpdateEmployeeAdminDTO } from './dto/update-employee-admin.dto';
+import { UpdateEmployeeDTO } from './dto/update-employee.dto';
 import { Employee } from './entities/employee.entity';
 
 @Injectable()
@@ -22,7 +29,7 @@ export class EmployeesService {
       cpf: createEmployeeDTO.cpf,
       email: createEmployeeDTO.email,
       name: createEmployeeDTO.name,
-      password: password_hash,
+      password_hash,
       role: createEmployeeDTO.role,
       situation: createEmployeeDTO.situation,
       phone_number: createEmployeeDTO.phone_number,
@@ -31,12 +38,231 @@ export class EmployeesService {
 
     const employeeCreate = this.employeeRepository.create(employeeCreateData);
 
-    const newEmployee = this.employeeRepository.save(employeeCreate);
+    const newEmployee = await this.employeeRepository.save(employeeCreate);
 
-    // Tirar o return na hora de finalizar o projeto
+    const allowedData = {
+      id: newEmployee.id,
+      email: newEmployee.email,
+      name: newEmployee.name,
+      role: newEmployee.role,
+    };
 
     return {
-      ...newEmployee,
+      ...allowedData,
     };
+  }
+
+  // Verificar se é um funcionário ou admin no guard ou middleware
+  async UpdateSelf(id: string, updateEmployeeDTO: UpdateEmployeeDTO) {
+    const allowedData = {
+      email: updateEmployeeDTO.email,
+      name: updateEmployeeDTO.name,
+      password_hash: updateEmployeeDTO.password,
+      phone_number: updateEmployeeDTO.phone_number,
+      address: updateEmployeeDTO.address,
+    };
+
+    // if (id !== tokenPayload.sub) {
+    //   throw new ForbiddenException('Ação não permitida');
+    // }
+
+    if (updateEmployeeDTO?.password) {
+      const passwordHash = await this.hashingService.Hash(
+        updateEmployeeDTO.password,
+      );
+
+      allowedData.password_hash = passwordHash;
+    }
+
+    const findEmployeeById = await this.employeeRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!findEmployeeById) {
+      throw new NotFoundException('Funcionário não encontrado');
+    }
+
+    const employeeUpdate = await this.employeeRepository.preload({
+      id,
+      ...allowedData,
+    });
+
+    if (!employeeUpdate) {
+      throw new InternalServerErrorException(
+        'Erro ao tentar atualizar funcionário',
+      );
+    }
+
+    return this.employeeRepository.save(employeeUpdate);
+  }
+
+  async UpdateAdmin(
+    id: string,
+    updateEmployeeAdminDTO: UpdateEmployeeAdminDTO,
+  ) {
+    const allowedData = {
+      email: updateEmployeeAdminDTO.email,
+      name: updateEmployeeAdminDTO.name,
+      password_hash: updateEmployeeAdminDTO.password,
+      role: updateEmployeeAdminDTO.role,
+      situation: updateEmployeeAdminDTO.situation,
+      phone_number: updateEmployeeAdminDTO.phone_number,
+      address: updateEmployeeAdminDTO.address,
+    };
+
+    const findEmployeeById = await this.employeeRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!findEmployeeById) {
+      throw new NotFoundException('Funcionário não encontrado');
+    }
+
+    const employeeUpdate = await this.employeeRepository.preload({
+      id,
+      ...allowedData,
+    });
+
+    if (!employeeUpdate) {
+      throw new InternalServerErrorException(
+        'Erro ao tentar atualizar dados de funcionário',
+      );
+    }
+
+    return this.employeeRepository.save(employeeUpdate);
+  }
+
+  // Validar os dados dos parametros (e de onde mais eles vierem. Para todas as buscas)
+  async FindByCpf(cpf: string) {
+    const employeeFindByCpf = await this.employeeRepository.findOneBy({
+      cpf,
+    });
+
+    if (!employeeFindByCpf) {
+      throw new NotFoundException('Funcionário não encontrado');
+    }
+
+    return employeeFindByCpf;
+  }
+
+  async FindByEmail(email: string) {
+    const employeeFindByEmail = await this.employeeRepository.findOneBy({
+      email,
+    });
+
+    if (!employeeFindByEmail) {
+      throw new NotFoundException('Funcionário não encontrado');
+    }
+
+    return employeeFindByEmail;
+  }
+
+  async FindByName(paginationDTO: PaginationDTO) {
+    const { limit, offset, value } = paginationDTO;
+
+    const employeeFindByName = await this.employeeRepository.find({
+      take: limit,
+      skip: offset,
+      order: {
+        id: 'desc',
+      },
+      where: {
+        name: Like(`${value}%`),
+      },
+    });
+
+    if (!employeeFindByName) {
+      throw new InternalServerErrorException(
+        'Erro desconhecido ao tentar pesquisar por funcionários',
+      );
+    }
+
+    if (employeeFindByName.length < 1) {
+      throw new NotFoundException('Funcionários não encontrados');
+    }
+
+    return employeeFindByName;
+  }
+
+  async FindByPhoneNumber(phoneNumber: string) {
+    const employeeFindByPhoneNumber = await this.employeeRepository.findOneBy({
+      phone_number: phoneNumber,
+    });
+
+    if (!employeeFindByPhoneNumber) {
+      throw new NotFoundException('Funcionário não encontrado');
+    }
+
+    return employeeFindByPhoneNumber;
+  }
+
+  async FindByAddress(address: string) {
+    const employeeFindByAddress = await this.employeeRepository.findOneBy({
+      address: address,
+    });
+
+    if (!employeeFindByAddress) {
+      throw new NotFoundException('Funcionário não encontrado');
+    }
+
+    return employeeFindByAddress;
+  }
+
+  async FindByRole(paginationDTO: PaginationDTO) {
+    const { limit, offset, value } = paginationDTO;
+
+    const employeeFindByRole = await this.employeeRepository.find({
+      take: limit,
+      skip: offset,
+      order: {
+        id: 'desc',
+      },
+      where: {
+        role: value,
+      },
+    });
+
+    if (!employeeFindByRole) {
+      throw new InternalServerErrorException(
+        'Erro desconhecido ao tentar pesquisar por funcionários',
+      );
+    }
+
+    if (employeeFindByRole.length < 1) {
+      throw new NotFoundException('Funcionários não encontrados');
+    }
+
+    return employeeFindByRole;
+  }
+
+  async FindBySituation(paginationDTO: PaginationDTO) {
+    const { limit, offset, value } = paginationDTO;
+
+    const employeeFindBySituation = await this.employeeRepository.find({
+      take: limit,
+      skip: offset,
+      order: {
+        id: 'desc',
+      },
+      where: {
+        situation: value,
+      },
+    });
+
+    if (!employeeFindBySituation) {
+      throw new InternalServerErrorException(
+        'Erro desconhecido ao tentar pesquisar por funcionários',
+      );
+    }
+
+    if (employeeFindBySituation.length < 1) {
+      throw new NotFoundException('Funcionários não encontrados');
+    }
+
+    return employeeFindBySituation;
   }
 }
