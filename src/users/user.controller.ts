1e5f8437c37ef6c0e4963ea362c70a3405b9b6e8
lucasNasc <lucasfortunato328@gmail.com>
2025-12-03 13:13:26 -0300

    adicionando permissoes para rotas de busca de usuarios

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -6,12 +6,16 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { Public } from 'src/auth/decorators/set-metadata.decorator';
+import { SetRoutePolicy } from 'src/auth/decorators/set-route-policy.decorator';
 import { TokenPayloadDTO } from 'src/auth/dto/token-payload.dto';
+import { RoutePolicyGuard } from 'src/auth/guards/route-policy.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
 import { UpdateUuidDTO } from 'src/common/dto/update-uuid.dto';
+import { EmployeeRole } from 'src/common/enums/employee-role.enum';
 import { ReqBodyPhoneNumberValidation } from 'src/common/pipes/phone-number-validation-body-request.pipe';
 import { PaginationByNameDTO } from '../common/dto/pagination-name.dto';
 import { CreateUserDTO } from './dto/create-user.dto';
@@ -40,12 +44,18 @@ export class UsersController {
     return this.usersService.Update(id, updateUserDTO, tokenPayloadDTO);
   }
 
+  @Public()
+  @UseGuards(RoutePolicyGuard)
   @Get('search/email/:email')
+  @SetRoutePolicy(EmployeeRole.ADMIN)
   FindByEmail(@Param('email') email: SearchByEmailDTO) {
     return this.usersService.FindByEmail(email);
   }
 
+  @Public()
+  @UseGuards(RoutePolicyGuard)
   @Get('search/name/')
+  @SetRoutePolicy(EmployeeRole.ADMIN)
   FindByName(@Query() paginationByNameDto: PaginationByNameDTO) {
     return this.usersService.FindByName(paginationByNameDto);
   }
