abc3885b161b1dee6379150264b41880f38a6a1b
lucasNasc <lucasfortunato328@gmail.com>
2025-12-04 17:58:21 -0300

    retornando cookies na reautenticacao

--- src/refresh-tokens/refresh-token.controller.ts
+++ src/refresh-tokens/refresh-token.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { Public } from 'src/auth/decorators/set-metadata.decorator';
 import { RefreshTokenGuard } from 'src/auth/guards/refresh-token.guard';
 import { RefreshTokensService } from './refresh-token.service';
@@ -11,14 +11,59 @@ export class RefreshTokensController {
   constructor(private readonly refreshTokensService: RefreshTokensService) {}
 
   @Post('user')
-  RefreshTokensUser(@Req() req: Request) {
+  async RefreshTokensUser(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const token = (req as any).refreshToken;
-    return this.refreshTokensService.RefreshTokensUser(token);
+
+    const getRefreshToken =
+      await this.refreshTokensService.RefreshTokensUser(token);
+
+    res.cookie('accessToken', getRefreshToken.accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 15, // 15 minutos
+      path: '/',
+    });
+
+    res.cookie('refreshToken', getRefreshToken.refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+      path: '/auth/refresh',
+    });
+
+    return { success: true, message: 'Reutenticação concluída' };
   }
 
   @Post('employee')
-  RefreshTokensEmployee(@Req() req: Request) {
+  async RefreshTokensEmployee(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const token = (req as any).refreshToken;
-    return this.refreshTokensService.RefreshTokensEmployee(token);
+    const getRefreshToken =
+      await this.refreshTokensService.RefreshTokensEmployee(token);
+
+    res.cookie('accessToken', getRefreshToken.accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 15, // 15 minutos
+      path: '/',
+    });
+
+    res.cookie('refreshToken', getRefreshToken.refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+      path: '/auth/refresh',
+    });
+
+    return { success: true, message: 'Reutenticação concluída' };
   }
 }
