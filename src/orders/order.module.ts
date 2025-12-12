0d5682625f155615592c67f0d1b5f651b22bc613
lucasNasc <lucasfortunato328@gmail.com>
2025-12-12 13:55:45 -0300

    adicionando entidade Product no modulo de orders

--- src/orders/order.module.ts
+++ src/orders/order.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
 import { UsersModule } from 'src/users/user.module';
 import { Items } from './entities/items.entity';
 import { Order } from './entities/order.entity';
@@ -7,7 +8,7 @@ import { OrdersController } from './order.controller';
 import { OrdersService } from './order.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Items]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Order, Items, Product]), UsersModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
