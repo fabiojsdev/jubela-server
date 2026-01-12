d1b95a37ae7c393002fd80a6b8448d0895338233
lucasNasc <lucasfortunato328@gmail.com>
2026-01-12 13:39:13 -0300

    emailControllerTemp

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -2,10 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from 'src/orders/entities/order.entity';
 import { OrdersModule } from 'src/orders/order.module';
+import { EmailControllerTemp } from './email.controller';
 import { EmailService } from './email.service';
 
 @Module({
   providers: [EmailService],
+  controllers: [EmailControllerTemp],
   exports: [EmailService],
   imports: [TypeOrmModule.forFeature([Order]), forwardRef(() => OrdersModule)],
 })
