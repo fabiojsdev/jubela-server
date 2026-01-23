a94e4f82fddcb89571e14aa7e1883a21ec1860ee
lucasNasc <lucasfortunato328@gmail.com>
2026-01-23 18:49:32 -0300

    rota create temporaria orders alteracao desfeita

--- src/orders/order.service.ts
+++ src/orders/order.service.ts
@@ -12,8 +12,8 @@ import Decimal from 'decimal.js';
 import { TokenPayloadDTO } from 'src/auth/dto/token-payload.dto';
 import { OrderStatus } from 'src/common/enums/order-status.enum';
 import { EmailService } from 'src/email/email.service';
-import { Employee } from 'src/employees/entities/employee.entity';
 import { Product } from 'src/products/entities/product.entity';
+import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/user.service';
 import { DataSource, LessThan, Repository } from 'typeorm';
 import { CreateOrderItemDTO } from './dto/create-item.dto';
@@ -45,7 +45,7 @@ export class OrdersService {
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
-    let findUser: Employee;
+    let findUser: User;
     let newOrderData: Order;
     let findProduct: Product;
     let sendEmail = false;
@@ -56,7 +56,7 @@ export class OrdersService {
         new Decimal(0),
       );
 
-      findUser = await queryRunner.manager.findOne(Employee, {
+      findUser = await queryRunner.manager.findOne(User, {
         where: {
           id: tokenPayloadDTO.sub,
         },
