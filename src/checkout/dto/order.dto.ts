2be8ba5b1841c62326b1e8bb2612141e446147df
lucasNasc <lucasfortunato328@gmail.com>
2026-02-23 12:48:31 -0300

    validacao de pedidos

--- src/checkout/dto/order.dto.ts
+++ src/checkout/dto/order.dto.ts
@@ -1,5 +1,12 @@
+import { Type } from 'class-transformer';
+import { IsArray, ValidateNested } from 'class-validator';
 import { CreateOrderItemDTO } from 'src/orders/dto/create-item.dto';
 
 export class OrderDTO {
+  @IsArray({
+    message: 'Pedidos deve ser um array',
+  })
+  @ValidateNested({ each: true }) // <--- ESSENCIAL: Valida cada item do array
+  @Type(() => CreateOrderItemDTO)
   orderItems: CreateOrderItemDTO[];
 }
